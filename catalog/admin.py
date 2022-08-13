dd50a248689c95c13847ef1019e37e846d4244ca
alifhs <alif.hs@outlook.com>
2022-08-13 22:22:56 +0600

    add decorator

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from .models import Author, Genre, Book, BookInstance
 
-admin.site.register(Book)
-admin.site.register(Author)
+# Define the admin class
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+# Register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)
+
+
+# admin.site.register(Book)
+# admin.site.register(Author)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
+# admin.site.register(BookInstance)
 
 # Register your models here.
