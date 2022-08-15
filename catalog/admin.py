0bed48b764f3c9f03cd0c9f574f7cbc2b9d7549d
alifhs <alif.hs@outlook.com>
2022-08-15 17:29:12 +0600

    add list_display on admin panel

--- catalog/admin.py
+++ catalog/admin.py
@@ -2,12 +2,27 @@ from django.contrib import admin
 from .models import Author, Genre, Book, BookInstance
 
 # Define the admin class
+@admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
 
 # Register the admin class with the associated model
-admin.site.register(Author, AuthorAdmin)
+# admin.site.register(Author, AuthorAdmin)
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
 
+
+
+# Register the Admin classes for BookInstance using the decorator
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status',  'due_back', 'id')
+    list_filter = ('status', 'due_back')
 
 # admin.site.register(Book)
 # admin.site.register(Author)
